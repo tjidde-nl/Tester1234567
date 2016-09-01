f000366c1b4fac26668e5184a55553606a5707b5
tjidde-nl <tjidde@tjidde.nl>
2016-09-01 17:29:20 +0200

    Testclasse added

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/AwesomeClass.cs
@@ -0,0 +1,19 @@
+﻿namespace WindowsFormsApplication1
+{
+    public class AwesomeClass
+    {
+        public bool test1(int a)
+        {
+            if (a == 1)
+                return true;
+            return false;
+        }
+
+        public int test2(bool a)
+        {
+            if (a)
+                return 1;
+            return 2;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1Tests/AwesomeClassTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Tests
+{
+    [TestClass()]
+    public class AwesomeClassTests
+    {
+        AwesomeClass ac = new AwesomeClass();
+        [TestMethod()]
+        public void test1Test()
+        {
+            Assert.IsFalse(ac.test1(2));
+            Assert.IsTrue(ac.test1(1));
+        }
+
+        [TestMethod()]
+        public void test2Test()
+        {
+            Assert.Equals(ac.test2(true), 1);
+            Assert.Equals(ac.test2(false), 2);
+        }
+    }
+}
